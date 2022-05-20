6fac3867b654c4b4eaa6ca1b14552d257d9f3b17
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2022-05-20 18:15:27 +0200

    created 128x128 versions of the insignias for use in the branch selector

--- src/components/branchSelect.jsx
+++ src/components/branchSelect.jsx
@@ -6,7 +6,7 @@ const BranchSelect = ({ militaryBranches }) => (
         {militaryBranches.length > 0 ? militaryBranches.map((branch, index) => (
           <a key={index} href={`/branch/${branch.path}`}>
             <div>
-              <img src={`/img/insignia/${branch.seal}`} alt={branch.name} />
+              <img src={`/img/insignia/128/${branch.seal}`} alt={branch.name} />
               <span>{branch.name}</span>
             </div>
           </a>
