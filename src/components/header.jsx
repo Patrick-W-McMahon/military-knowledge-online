7e312242860278804cb1f28e8659f12d455810e7
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2022-05-23 22:30:23 +0200

    added email button on header bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,6 +9,7 @@ const Header = ({ siteTitle, militaryBranches }) => (
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
+        <Nav.Link rel="noreferrer" target="_blank" href="https://webmail.apps.mil/"><i className="far fa-envelope fa-2x btn btn-primary email-link"></i></Nav.Link>
         {/*
         <Nav.Link href="/cards">Cards</Nav.Link>
         */}
