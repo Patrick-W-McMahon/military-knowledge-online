35c331bed72c5881425edae293b4b204ff6d8614
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2022-09-01 23:02:29 +0200

    refactored and code cleanup for LinkList component

--- src/components/linkList.jsx
+++ src/components/linkList.jsx
@@ -6,18 +6,13 @@ const LinkList = ({ links }) => (
     <Fragment>
         {links[0]?.links ? abc.map((a,i) => {
             const group = links.find(g => g.fieldValue === a);
-            console.log('list test: ', group);
             return (
                 <details key={`link-${i}`} open>
                     <summary><b> - {a.toUpperCase()} - </b></summary>
-                    {group && group.links && group.links.length ? group.links.map((link,index) => (
-                        <LinkCmp key={index} link={link} />
-                    )): <div>No data</div>}
+                    {group && group.links && group.links.length ? group.links.map((link,index) => <LinkCmp key={index} link={link} />): <div>No data</div>}
                 </details>
             );
-        }) : links.map((link, index) => (
-            <LinkCmp key={index} link={link} />
-        ))}
+        }) : links.map((link, index) => <LinkCmp key={index} link={link} />)}
     </Fragment>
 );
 
