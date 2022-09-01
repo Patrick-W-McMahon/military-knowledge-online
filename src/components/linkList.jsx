95d98ebf17bbefc1ce86a8af865438768671755e
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2022-09-01 22:58:43 +0200

    fixed linkList component not showing the 'No data' message when no links are provided.

--- src/components/linkList.jsx
+++ src/components/linkList.jsx
@@ -6,10 +6,11 @@ const LinkList = ({ links }) => (
     <Fragment>
         {links[0]?.links ? abc.map((a,i) => {
             const group = links.find(g => g.fieldValue === a);
+            console.log('list test: ', group);
             return (
                 <details key={`link-${i}`} open>
                     <summary><b> - {a.toUpperCase()} - </b></summary>
-                    {group ? group.links.map((link,index) => (
+                    {group && group.links && group.links.length ? group.links.map((link,index) => (
                         <LinkCmp key={index} link={link} />
                     )): <div>No data</div>}
                 </details>
