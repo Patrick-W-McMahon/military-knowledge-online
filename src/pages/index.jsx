3e54b85cd37f8679ace181d143bd82c3c1da196a
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2022-09-11 13:29:15 +0200

    updating the front page info

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ const IndexPage = () => (
                 <Modal.Title>MKO just got an update, enjoy! </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p>Exciting new features just for you. You can now save links that you use regularly to your favorites.</p>
+                <p>Exciting new features! You can now save links that you use regularly to your favorites and set what filter you want as your default.</p>
                 <p>You will find the new configuration button in your workspace. Toggle the “Link Edit Mode” and you will notice a star icon in the top right of each card. Clicking on these will add them to your favorite list. To view your favorite list select favorites in the filter groups dropdown.</p>
                 <p>All saved data is stored locally to your browser and is not sent out from your computer. Clearing your browser cache will delete your profile settings.  </p>
             </Modal.Body>
