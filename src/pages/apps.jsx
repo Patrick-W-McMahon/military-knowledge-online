71f5a4812174f8c501d940d1dbab1a10ece4d6d2
Patrick-W-McMahon <patrick.w.mcmahon@gmail.com>
2024-04-07 14:48:30 +0200

    temp fix for mobile app tray
    temp fix to hid side bar and make tray full size and active.

--- src/pages/apps.jsx
+++ src/pages/apps.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 //import { connect } from "react-redux";
 import { useStaticQuery, graphql } from "gatsby";
 import { Container, Row, Col, Nav } from 'react-bootstrap';
@@ -23,46 +23,48 @@ const AppsPage = ({ selectedContentPanel }) => {
     `);
 
     const { appsList } = allApplication;
-    console.log("appsList", appsList);
 
     return (
-        <MainLayout activePos={2} nonScroll>
-            <MainLayout.Navigation>
-                <Nav className="me-auto"></Nav>
-            </MainLayout.Navigation>
-            <Container fluid>
-                <Row>
-                    <Col md="2" className={`page-menu${selectedContentPanel===0 ? ' active' : ''}`}>
-                        <div>Side nav data not accessible</div>
-                    </Col>
-                    <Col md="10" className={`body-page${selectedContentPanel===1 ? ' active' : ''}`}>
-                        {/*<Row className="justify-content-md-center">
-                            <Col xs={6} md={6} className="alert alert-warning text-center">
-                                <h1 className="w-100 p-3"><i className="fas fa-tools fa-lg"></i> Page under construction <i className="fas fa-tools fa-lg"></i></h1>
-                                <hr/>
-                                <p>This page is under construction. Please check back later.</p>
-                                <p>The apps page will hold all your applications</p>
-                            </Col>
-                        </Row>*/}
-                        <Container fluid className="app-tray">
-                            <Row>
-                                {appsList.map((app, index) => {
-                                    const { app_name, app_icon, appURI } = app;
-                                    return (
-                                        <Col key={index} className="appIcon" xs lg="1">
-                                            <a className="" rel="noreferrer" target="_blank" href={appURI}>
-                                                <img src={`/img/appIcons/${app_icon}.png`} alt={app_name} />
-                                                <span>{app_name}</span>
-                                            </a>
-                                        </Col>
-                                    );
-                                })}
-                            </Row>
-                        </Container>
-                    </Col>
-                </Row>
-            </Container>
-        </MainLayout>
+        <Fragment>
+            <MainLayout activePos={2} nonScroll>
+                <MainLayout.Navigation>
+                    <Nav className="me-auto"></Nav>
+                </MainLayout.Navigation>
+                <Container fluid>
+                    <Row>
+                        {/*<Col md="2" className={`page-menu${selectedContentPanel===0 ? ' active' : ''}`}>
+                            <div>Side nav data not accessible</div>
+                        </Col>*/}
+                        {/*<Col md="10" className={`body-page${selectedContentPanel===1 ? ' active' : ''}`}>*/}
+                        <Col md="12" className={`body-page active`}>
+                            {/*<Row className="justify-content-md-center">
+                                <Col xs={6} md={6} className="alert alert-warning text-center">
+                                    <h1 className="w-100 p-3"><i className="fas fa-tools fa-lg"></i> Page under construction <i className="fas fa-tools fa-lg"></i></h1>
+                                    <hr/>
+                                    <p>This page is under construction. Please check back later.</p>
+                                    <p>The apps page will hold all your applications</p>
+                                </Col>
+                            </Row>*/}
+                            <Container fluid className="app-tray">
+                                <Row>
+                                    {appsList.map((app, index) => {
+                                        const { app_name, app_icon, appURI } = app;
+                                        return (
+                                            <Col key={index} className="appIcon" xs={3} md={1} lg={1}>
+                                                <a className="" rel="noreferrer" target="_blank" href={appURI}>
+                                                    <img src={`/img/appIcons/${app_icon}.png`} alt={app_name} />
+                                                    <span>{app_name}</span>
+                                                </a>
+                                            </Col>
+                                        );
+                                    })}
+                                </Row>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
+            </MainLayout>
+        </Fragment>
     );
 }
 
