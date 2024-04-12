9043ac669e07f936bfed2b4926b6575bcfc058cf
Patrick-W-McMahon <patrick.w.mcmahon@gmail.com>
2024-04-12 19:24:23 +0200

    added hover text on menu items

--- src/components/layout/comp/Sidebar.jsx
+++ src/components/layout/comp/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = ({ extended, handleSidebarToggle, menuItems, activePos }) => {
         <Fragment>
             <div className={`sidebar${extended ? ' extended' : ''}`}>
                 <Nav className="flex-column">
-                    {menuItems.map((item, index) => <Link key={index} to={item.url} className={`nav-link${activePos === index ? ' active' : ''}`}><i className={`fas ${item.icon} fa-lg`}></i> <span className={`${extended ? '' : 'hidden'}`}>{item.text}</span></Link>)}
+                    {menuItems.map((item, index) => <Link key={index} title={item.text} to={item.url} className={`nav-link${activePos === index ? ' active' : ''}`}><i className={`fas ${item.icon} fa-lg`}></i> <span className={`${extended ? '' : 'hidden'}`}>{item.text}</span></Link>)}
                 </Nav>
             </div>
             <button className={'sidebar-toggle-btn'} onClick={handleSidebarToggle}><span>{'|'}</span></button>
